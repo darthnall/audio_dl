ef923bd1f2e5a9c0c43384edac423c61bd7c1e96
darthnall <blakenall@proton.me>
2023-09-15 11:17:39 -0500

    Fixed git

--- audio_dl/reformat.py
+++ audio_dl/reformat.py
@@ -1,6 +1,16 @@
-import ffmpeg
+from ffmpeg import FFmpeg
+import os
 
 def reformat_all_files(files, fmt):
     for file in files:
-        ffmpeg.output(ffmpeg.input(file), file, format=fmt)
-
+        output_dir = os.path.abspath(file).split("/").pop()
+        output_dir = "/".join(output_dir)
+        filename = file.split(".")[0]
+        ffmpeg = (
+                FFmpeg()
+                .input(file)
+                .output(
+                    f"{output_dir}/{filename}.{fmt}",
+                    )
+                )
+        ffmpeg.execute()
