c07f8a8f041683d123febf15b1197d057f771aac
darthnall <blakenall@proton.me>
2023-09-15 20:37:00 -0500

    Optimization

--- audio_dl/handlers.py
+++ audio_dl/handlers.py
@@ -1,35 +1,19 @@
 import ffmpeg, os
 from time import strftime, localtime
-from sys import exit
-from pathlib import Path
 
-def convert_files(files, fmt):
-    for file in files:
-        pass
-
-def rename_files(files):
-    for file in files:
-        new_name = "_".join(file.strip().lower().split(' '))
-        os.rename(file, new_name)
-    return os.listdir(".")
-
-# TODO: optimize this function
-def get_output():
+def ydl_init():
     time = strftime("%H%M%S", localtime())
-    timeout = 0
-
-    while timeout >= 3:
-        try:
-            os.chdir("./output")
-        except FileNotFoundError:
-            os.mkdir("./output")
-            os.chdir("./output")
-        finally:
-            timeout =+ 1
-
     os.mkdir(time)
-    output_path = Path(time)
-    output_str = os.fspath(output_path)
+    output_dir = os.fspath(time)
+    return output_dir
 
-    print(output_path, output_str) # TODO: Remove this when optimized
-    return output_path, output_str
+def progress_hooks(p, file):
+    if p["status"] == "downloading":
+        pass
+    if p["status"] == "finished":
+        rename_file(file)
+
+def rename_file(file):
+    new_name = "_".join(file.strip().lower().split(' '))
+    os.rename(file, new_name)
+    return new_name
