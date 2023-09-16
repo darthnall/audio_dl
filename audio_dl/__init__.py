483d52b5b7bbb8bf4d137b0a319220f36f29cd9c
darthnall <blakenall@proton.me>
2023-09-15 20:36:48 -0500

    Optimization

--- audio_dl/__init__.py
+++ audio_dl/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 import youtube_dl
-from .handlers import get_output, rename_files
+from .handlers import ydl_init
+from .handlers import progress_hooks
+from .handlers import rename_file
 
 def start():
     valid_formats = ["m4a", "mp4", "mp3", "ogg", "wav", "webm"]
-    output = get_output()
+
     while True:
         fmt = input(f"Valid formats: {valid_formats}\nFormat: ")
         if fmt in valid_formats:
@@ -12,28 +14,17 @@ def start():
         else:
             print("Please select a valid format.")
 
+    output_dir = ydl_init()
+
     ydl_opts = {
             "format": f"bestaudio[ext={fmt}]/bestaudio/m4a",
+            "outmmpl": f".{output_dir}/%(title)s%(ext)s",
+            "progress_hooks": [progress_hooks],
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
                 "preferredquality": "192",
                 }],
-            }
+    }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        url = str(input("Paste playlist: "))
-        ydl.download((url,))
-
-    files = rename_files(output)
-
-    invalid_files = []
-    for file in files:
-        ext = file.split(".")[-1]
-        if ext in valid_formats and ext == fmt:
-            pass
-        else:
-            invalid_files.append(file)
-
-        if invalid_files:
-            from handlers import convert_files
-            convert_files(invalid_files, fmt)
+        ydl.download((str(input("Paste playlist: "),)))
